c959ed7ae69da81c4346042805dad9eb573d4bde
John Webster <JohnLobster@comcast.net>
2019-04-02 15:49:24 -0700

    Added enemy selection page

--- assets/javascript/swGame.js
+++ assets/javascript/swGame.js
@@ -13,7 +13,7 @@ gamesLost = 0;
 
 // Select the hero (visible by default)
 
-console.log("Here");
+
 $(".swGameCard").on("click", function(){
     var cardSelected = this.id;
     console.log("Selected " + cardSelected);
@@ -37,11 +37,60 @@ $(".swGameCard").on("click", function(){
     // hide gameSelection as finished with it
     $("#gameSelection").hide();
     // now duplicate the remaining cards and attach them into the selectEnemy DIV
-    heroCard = $(this).clone();
+    var heroCard = $(this).clone();
     heroCard.addClass("swHero"); 
     $("#hero").append(heroCard);
+    $("#selectEnemy").show(); // display
     // add the enemy cards
-    $("#selectEnemy").show();
+    $(".swGameCard").each( function(i, e){
+        // console.log( $(e).attr("id"));
+        if ( $(e).attr("id") !== cardSelected) {
+            var enemyCard = $(e).clone();
+            enemyCard.addClass("swEnemy");
+            $("#enemies").append(enemyCard);
+        }
+    });
+    
+
+    // now set up event for clicking enemy cards 
+    $(".swEnemy").on("click", function() {
+        var cardSelected = this.id;
+        console.log("Enemy Selected " + cardSelected);
+        // run through all the game cards
+        $(".swGameCard").each( function(i, e){
+            // console.log( $(e).attr("id"));
+            if ( $(e).attr("id") !== cardSelected) {
+         
+                $(e).animate( {opacity:0.8}, "slow");
+                $(e).animate( {opacity:0.6}, "slow");
+                $(e).animate( {opacity:0.4}, "slow");
+                $(e).animate( {opacity:0.2}, "slow");
+                // doesn't seem to wait for animations to complete
+            }
+            // turn off these buttons so can't click twice
+            $(e).off("click"); 
+        } );
+        $("#selectEnemy").hide(); // finished Enemy selection
+        // set up fight cards
+        var enemyCard = $(this).clone();
+        enemyCard.addClass("enemyCard");
+        enemyCard.stop();
+        heroCard.stop();
+        enemyCard.css( "opacity", 1);
+        heroCard.css( "opacity", 1);
+
+        // build fighting screen
+        $("#heroColumn").append( heroCard);
+        $("#enemyColumn").append( enemyCard)
+
+        $("#gamePlay").show();
+        
+        
+    });
+
+    
+    
+
 
 });
 
